71b9cc1f901e335a7c7877326e933da66ec70dc7
suryaraj1 <suryaraj_bhaduri@yahoo.com>
2021-05-25 11:47:19 +0530

    started react files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
