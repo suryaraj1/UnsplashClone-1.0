236a0572ae1faa4c778247116bc6b63968d7e211
suryaraj1 <suryaraj_bhaduri@yahoo.com>
2021-05-25 12:43:32 +0530

    add navbar changes

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Navbar.css";
+import PhotoForm from "../PhotoForm/PhotoForm";
+import UnsplashLogo from "../../static/my_unsplash_logo.svg";
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  clickHandler = () => {
+    this.setState({
+      showModal: !this.state.showModal,
+    });
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div className="navbar-wrapper">
+        <div className="navbar-logo-search-wrapper">
+          <img className="navbar-img" src={UnsplashLogo} />
+          <div className="navbar-search-wrapper">
+            <svg
+              className="navbar-search-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              className="navbar-search"
+              type="input"
+              placeholder="Search by name"
+            />
+          </div>
+        </div>
+        <div className="navbar-btn-wrapper">
+          <button className="navbar-btn" onClick={this.clickHandler}>
+            Add a Photo
+          </button>
+          <PhotoForm showModal={showModal} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
