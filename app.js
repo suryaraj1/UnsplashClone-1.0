32894fdb8570cdd9cc0c6a8974ea4b9f700d0f79
suryaraj1 <suryaraj_bhaduri@yahoo.com>
2021-05-27 11:37:52 +0530

    added heroku postbuild config and removed proxy

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 app.use("/api/photos", photos);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
